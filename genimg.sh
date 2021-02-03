b6761a2142345d10c650a019cdeb145e184beac4
r3vn <giuseppe.corti@protonmail.com>
2021-02-03 01:44:45 +0100

    [rootfs-api28gsi-all] added package-sideload support, output rootfs as ready-to-use img

---
+++ genimg.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+ROOTFS_PATH=$(find . -maxdepth 1 -mindepth 1 -type d -name .debos-*)/root
+ROOTFS_SIZE=$(du -sm $ROOTFS_PATH | awk '{ print $1 }')
+
+IMG_NAME=$1
+IMG_SIZE=$(( ${ROOTFS_SIZE} + 250 )) # FIXME 250MB contingency
+IMG_MOUNTPOINT=".image"
+
+# create root img
+echo "Creating an empty root image"
+dd if=/dev/zero of=$IMG_NAME bs=1M count=${IMG_SIZE}
+mkfs.ext4 $IMG_NAME
+
+# mount the image
+echo "Mounting root image"
+mkdir -p $IMG_MOUNTPOINT
+mount -o loop $IMG_NAME ${IMG_MOUNTPOINT}
+
+# copy rootfs content
+echo "Syncing rootfs content"
+rsync --archive -H -A -X $ROOTFS_PATH/* ${IMG_MOUNTPOINT}
+sync
+echo "done."
+
+# umount the image
+echo "umount root image"
+umount $IMG_MOUNTPOINT
